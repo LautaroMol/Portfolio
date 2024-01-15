ff4eaf0a01ecd927e3f5f05b8be5794d6e38acdb
Lautaro <laumol159@gmail.com>
2024-01-15 20:38:30 -0300

    dependencias

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AboutMeComponent } from './app/about-me/about-me.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+  bootstrapApplication(AboutMeComponent, appConfig)
   .catch((err) => console.error(err));
